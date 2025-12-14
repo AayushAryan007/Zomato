776f2663569d07e656d290b63d424a05ffa43c94
Aayush <unitedestates.ayush10@gmail.com>
2025-12-14 06:33:45 +0530

    food upload api complete

--- Server/src/controllers/food.controler.js
+++ Server/src/controllers/food.controler.js
@@ -2,14 +2,25 @@ const foodModel = require("../models/food.model");
 const storageService = require("../services/storage.service");
 const { v4: uuidv4 } = require("uuid");
 async function createFood(req, res) {
-  res.send("Create Food Endpoint");
+  // res.send("Create Food Endpoint");
   // console.log(req.body);
   // console.log(req.file);
   const fileUploadResult = await storageService.uploadFile(
     req.file.buffer,
     uuidv4()
   );
-  console.log(fileUploadResult);
+  const foodItem = await foodModel.create({
+    name: req.body.name,
+    description: req.body.description,
+    video: fileUploadResult.url,
+    foodPartner: req.foodPartner._id,
+  });
+
+  // console.log(fileUploadResult);
+  res.status(201).json({
+    message: "Food created successfully",
+    food: foodItem,
+  });
 }
 
 module.exports = {
